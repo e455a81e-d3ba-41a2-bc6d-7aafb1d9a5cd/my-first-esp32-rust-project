bc6bf4d472337afc06509eca6c5710b75939b119
Leonhardt Schwarz <lsdev@posteo.de>
2024-12-01 20:20:50 +0100

    Reads DHT22 sensor and displays it on I2C display

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,7 @@ use ssd1306::{prelude::*, I2CDisplayInterface, Ssd1306};
 use esp_idf_hal::i2c::*;
 use esp_idf_hal::prelude::*;
 use esp_idf_hal::peripherals::Peripherals;
+use dht_embedded::{Dht22, DhtSensor, NoopInterruptControl};
 
 use embedded_graphics::{
     prelude::*,
@@ -10,6 +11,8 @@ use embedded_graphics::{
     mono_font::{ascii::FONT_6X10, MonoTextStyleBuilder},
     primitives::{PrimitiveStyleBuilder, Rectangle}
 };
+use esp_idf_hal::delay::Delay;
+use esp_idf_hal::gpio::PinDriver;
 
 fn main() -> anyhow::Result<()>
 {
@@ -21,15 +24,17 @@ fn main() -> anyhow::Result<()>
     esp_idf_svc::log::EspLogger::initialize_default();
 
     let peripherals = Peripherals::take()?;
-    let scl = peripherals.pins.gpio22;
-    let sda = peripherals.pins.gpio21;
+    let scl = peripherals.pins.gpio7;
+    let sda = peripherals.pins.gpio5;
     let i2c0 = peripherals.i2c0;
+    let pin10 = peripherals.pins.gpio10;
+    let pin_10_driver = PinDriver::input_output(pin10);
 
+    let mut sensor = Dht22::new(NoopInterruptControl, Delay::default(), pin_10_driver?);
 
     let config = I2cConfig::new().baudrate(100.kHz().into());
     let i2c = I2cDriver::new(i2c0, sda, scl, &config)?;
     let i2c_display_interface = I2CDisplayInterface::new(i2c);
-
 
     let display_size = DisplaySize128x64;
     let display_rotation = DisplayRotation::Rotate0;
@@ -60,7 +65,21 @@ fn main() -> anyhow::Result<()>
 
     loop {
         display.flush().unwrap();
-        log::info!("Hello, world!");
+
+        match sensor.read() {
+            Ok(reading) =>{
+                log::info!("{}°C, {}% RH", reading.temperature(), reading.humidity());
+                let message= format!("{}°C, {}% RH", reading.temperature(), reading.humidity());
+
+                display.clear(BinaryColor::Off).unwrap();
+                Text::with_baseline(message.as_str(), Point::new(0, 16), text_style, Baseline::Top)
+                    .draw(&mut display)
+                    .unwrap();
+                display.flush().unwrap();
+            }
+            Err(e) => log::error!("Error: {}", e),
+        }
+
         std::thread::sleep(std::time::Duration::from_secs(1));
     }
 }
